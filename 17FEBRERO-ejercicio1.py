d11c6671c5ba60ba56d08b20e16cf0fc6cbdd840
JorgeQMTZ <98997614+JorgeQMTZ@users.noreply.github.com>
2022-02-20 18:53:56 -0600

    Add files via upload

---
+++ 17FEBRERO-ejercicio1.py
@@ -0,0 +1,14 @@
+class GestionNumerica():
+    Operador1 = 0
+    Operador2 = 0    
+    def Sumar(self):
+        return self.Operador1 + self.Operador2
+    def Multiplicar(self):
+        return self.Operador1 * self.Operador2
+
+mi_objeto = GestionNumerica()
+mi_objeto.Operador1 = 10
+mi_objeto.Operador2 = 5
+
+print(mi_objeto.Operador1)
+print(mi_objeto.Operador2)
